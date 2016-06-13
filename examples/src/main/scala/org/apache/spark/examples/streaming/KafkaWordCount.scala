f621f7f525f1e93d1e14cbdf171ef94706c482a2
Pei Sun <peis@alluxio.com>
2016-06-13 15:38:44 -0700

    add checkpoint dir

--- examples/src/main/scala/org/apache/spark/examples/streaming/KafkaWordCount.scala
+++ examples/src/main/scala/org/apache/spark/examples/streaming/KafkaWordCount.scala
@@ -41,8 +41,9 @@ import org.apache.spark.streaming.kafka._
  */
 object KafkaWordCount {
   def main(args: Array[String]) {
-    if (args.length < 4) {
-      System.err.println("Usage: KafkaWordCount <zkQuorum> <group> <topics> <numThreads>")
+    if (args.length < 5) {
+      System.err.println(
+        "Usage: KafkaWordCount <zkQuorum> <group> <topics> <numThreads> <checkpoint>")
       System.exit(1)
     }
 
@@ -51,7 +52,7 @@ object KafkaWordCount {
     val Array(zkQuorum, group, topics, numThreads) = args
     val sparkConf = new SparkConf().setAppName("KafkaWordCount")
     val ssc = new StreamingContext(sparkConf, Seconds(2))
-    ssc.checkpoint("checkpoint")
+    ssc.checkpoint(args(4))
 
     val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
     val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
